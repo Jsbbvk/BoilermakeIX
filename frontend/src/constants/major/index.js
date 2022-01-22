434bf3cc0f4074994e880b4da0124569a432cbaa
Jsbbvk <jsbbvk@gmail.com>
2022-01-22 10:25:04 -0500

    added json courses

---
+++ frontend/src/constants/major/index.js
@@ -0,0 +1,11 @@
+import COMP from './comp.json'
+import CS_CORE from './cs_core.json'
+import LANG from './culture_lang.json'
+import GEN_ED from './gen_ed.json'
+import GREAT_ISSUES from './great_issues.json'
+import LAB_SCI from './labsci.json'
+import MATH from './math.json'
+import MULTI_DIS from './multidis.json'
+import STATS from './stats.json'
+
+export default { COMP, CS_CORE, LANG, GEN_ED, GREAT_ISSUES, LAB_SCI, MATH, MULTI_DIS, STATS }
